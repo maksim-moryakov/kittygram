69b30f3402f4fd2e25a8ea84e8c30032ffac0700
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-04-27 12:35:35 +0300

    Доработки

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -6,10 +6,15 @@ from .models import Cat
 from .serializers import CatSerializer
 
 
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+
+
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,22 @@
 from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+from cats.views import cat_list, CatViewSet
 
-from cats.views import cat_list
+# Создаётся роутер
+router = SimpleRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
 
 urlpatterns = [
-   path('cats/', cat_list),
+   path('cats/', cat_list, name='cat_list'),
+   path('cat/<int:pk>/', ..., name='cat-detail'),
+   path('', include(router.urls)),
 ]
+
 
 
